5b6e85e875d5940b5a043f41b1a7ddc5df05300b
ultimate-sports <19kok3@wis.edu.hk>
2015-11-13 09:12:34 +0800

    update

---
+++ game/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/kajun_ko/pen/qOQoWR.
+
+ 
---
+++ game/js/index.js
@@ -0,0 +1,103 @@
+function kajungame () {
+  
+{
+  confirm("This is the stupid quiz...");
+}
+{
+var askname = prompt("What is your name?");
+alert("welcome" +  askname);
+}
+var name = prompt("ilk ilk ilk. What do cows drink?");
+if (name === "water")
+  {
+    alert("Well done!!!");
+  }
+  else
+  {
+    alert("it's water");
+  }
+var name = prompt("Before Mount Everest was discovered, what was the highest Mountain in the world?");
+if (name === "mount everest")
+  {
+    alert("You're on a roll!!!")
+  }
+  else
+  {
+    alert("it's mount everest")
+  }
+var name = prompt("Some months have 31 days, some months have 30 days. How many months have 28 days?");
+if (name === "all")
+  {
+    alert("Are you cheating?")
+  }
+  else
+  {
+    alert("all months have 28 days") 
+  }
+var name = prompt("Which is heavier, 100 pounds of rocks or 100 pounds of feather?");
+if (name === "they're the same")
+  {
+    alert("well done!!!");
+  }
+  else
+  {
+    alert("they're the same!!!");
+  }
+var name = prompt("How many times can you subtract 10 from 100?");
+if (name === "once")
+  {
+      alert("You're acing this!")
+  }
+else
+  {
+      alert("once")
+  }
+var name = prompt("If there are 6 apples and you take away 4, how many apples are there left?")
+if (name === "4")
+  {
+    alert("You are the smartest person i have ever met")
+  }
+else
+  {
+    alert("4")
+  }
+var name = prompt("A man claims he can predict the score of every football match, before it begins. How is this possible?") 
+if (name === "he predicts the score will be 0-0 beofre the game starts")
+  {
+    alert("No way... how did you get that?")
+  }
+else
+  {
+    alert("he predicts the score will be 0-0 before the game starts")
+  }
+var name = prompt("You're a bus driver. At the first stop 4 people get on. At the second stop 8 people get on, at the third stop 2 people get off and, at the forth stop everyone got off. The question is what color are the bus driver's eyes?")
+if (name === "The same as yours.")
+  {
+    alert("This is not challenging you is it?")
+  }
+else
+  {
+    alert("The same as yours.")
+  }
+var name = prompt("What looks like half an apple")
+if (name === "the other half")
+  {
+    alert("Well done!!! You got one more question left...")
+  }
+else
+  {
+    alert("You're hopeless!!! The answer is the other half. You have one more question to make amends...")
+  }
+var name = prompt("A man dressed in all black is walking down a country lane. Suddenly, a large black car without any lights on comes round the corner and screeches to a halt. How did the car know he was there?")
+if (name === "it was day time")
+  {
+    alert("well done!!! you sure ended this well!!!")
+  }
+else
+  {
+    alert("you are officially stupid...")
+  }
+{
+  confirm("Thankyou for doing my quiz" +  askname)
+}
+  }
